6f3d01fe07fca975066d5b4d2c7f389922556f37
David Diez <ddiezr@gmail.com>
2018-11-20 17:54:56 +0100

    Exercised Finished No refactorized

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,11 +1,18 @@
 const ajaxBitcoinRequest = () => {
-    axios.get(`http://api.coindesk.com/v1/bpi/historical/close.json`)
+    const bitCoinInfo = axios.create({
+        baseURL: 'http://api.coindesk.com/v1/bpi/historical/close.json',
+    });
+
+    bitCoinInfo.get()
         .then((bitcoinData) => {
             let data = bitcoinData.data.bpi;
-            drawChart(Object.values(data),Object.keys(data));
-
-            // renderHTMLIronhack(new CountryData(countryData.data[0].name, countryData.data[0].callingCodes[0]))
+            drawChart(Object.values(data), Object.keys(data));
+            paintValues(Object.values(data));;
         })
+        .catch(error => {
+            console.log(error);
+        });
+
 }
 const bitcoinDataClickHandler = () => {
     return ajaxBitcoinRequest();
@@ -15,7 +22,7 @@ const bitcoinDataClickHandler = () => {
 document.getElementById('btn-bitcoin').onclick = bitcoinDataClickHandler
 
 
-drawChart = (bitcoins,bitDates) => {
+drawChart = (bitcoins, bitDates) => {
     // Chart
     var ctx = document.getElementById("myChart").getContext('2d');
     var myChart = new Chart(ctx, {
@@ -44,4 +51,53 @@ drawChart = (bitcoins,bitDates) => {
             }
         }
     });
+}
+
+getDatesData = () => {
+    let dateFrom = document.querySelector('#date-from').value;
+    let dateTo = document.querySelector('#date-to').value;
+    return [dateFrom, dateTo]
+}
+
+getCurrentData = () => {
+    return currency = document.querySelector('#currency-selector').value;
+}
+
+paintValues = (arr) => {
+    document.querySelector('#max').innerHTML = Math.max(...arr);
+    document.querySelector('#min').innerHTML = Math.min(...arr);
+}
+
+
+
+document.getElementById('date-to').onchange = () => {
+    let arr = getDatesData();
+    let dateFrom = arr[0];
+    let dateTo = arr[1];
+
+    axios.get(`http://api.coindesk.com/v1/bpi/historical/close.json?start=${dateFrom}&end=${dateTo}`)
+        .then((bitcoinData) => {
+            let data = bitcoinData.data.bpi;
+            drawChart(Object.values(data), Object.keys(data));
+            paintValues(Object.values(data));;
+        })
+        .catch(error => {
+            console.log(error);
+        });
+}
+
+document.getElementById('currency-selector').onchange = () => {
+    let curr = getCurrentData();
+    let arr = getDatesData();
+    let dateFrom = arr[0];
+    let dateTo = arr[1];
+    axios.get(`http://api.coindesk.com/v1/bpi/historical/close.json?start=${dateFrom}&end=${dateTo}&currency=${curr}`)
+        .then((bitcoinData) => {
+            let data = bitcoinData.data.bpi;
+            drawChart(Object.values(data), Object.keys(data));
+            paintValues(Object.values(data));;
+        })
+        .catch(error => {
+            console.log(error);
+        });
 }
